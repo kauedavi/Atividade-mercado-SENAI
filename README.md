82892d2666e516e6dba039e48f55491ff666b0c6
KAUE DAVI ROCHA DE JESUS <kaue.jesus7@senaisp.edu.br>
2026-09-18 15:52:38 -0300

    Feat: implementando otimização

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Atividade-mercado-SENAI
+
+Nome: Kauê Davi
+
+Requisitos obrigatórios concluido.
--- app/src/main/java/com/example/Main.java
+++ app/src/main/java/com/example/Main.java
@@ -9,22 +9,24 @@ public class Main {
 
         int inicio;
         String nomeCliente;
+
         float valorCompra = 0f;
         float totalRelatorio = 0f;
         float maiorValor = 0f;
         float menorValor = 0f;
-        int continuarAcao = 0;
+
         int quantidadeCompras = 0;
 
-        System.out.println("Bem vindo ao sistema de Registro de pedidos!");
-        System.out.println("Deseja começar a registrar os pedidos dos clientes? 1 - sim 2- não");
+        System.out.println("Começando algoritmo...");
+        System.out.println("Iniciar? 1 - sim 2- não");
         inicio = input.nextInt();
 
         if (inicio == 1) {
+            System.out.println("Qual o nome do cliente?");
+            input.nextLine();
+            nomeCliente = input.nextLine();
+
             do {
-                System.out.println("Qual o nome do cliente?");
-                input.nextLine();
-                nomeCliente = input.nextLine();
 
                 do {
                     System.out.print("Qual o valor da compra?\nR$");
@@ -48,15 +50,10 @@ public class Main {
                 System.out.println("Valor da compra: R$" + valorCompra);
                 quantidadeCompras++;
 
-                do {
-                    System.out.println("Deseja cadastrar outro pedido?\n1 - Sim\n2 - Não\n");
-                    continuarAcao = input.nextInt();
-                    if (continuarAcao != 1 && continuarAcao != 2) {
-                        System.out.println("Opção inválida. Tente novamente");
-                    }
-                } while (continuarAcao != 1 && continuarAcao != 2);
-            } while (continuarAcao == 1);
+                System.out.println("Deseja cadastrar outro pedido?\n1 - Sim\n2 - Não\n");
+                inicio = input.nextInt();
 
+            } while (inicio == 1);
 
             System.out.println("RELATÓRIO DE COMPRAS");
             System.out.println("\nQuantidade de pedidos: " + quantidadeCompras);
